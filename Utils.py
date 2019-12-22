ee2c9d0a42ef6c8b0b6463f94d91ebc65f3f826a
mahmudz <mahmudbappy.pri@gmail.com>
2019-12-22 14:25:45 +0600

    Initial commmit

---
+++ Utils.py
@@ -0,0 +1,68 @@
+import sublime
+import sublime_plugin
+import re
+
+class SnakeCaseCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        selection = self.view.sel()
+        for region in selection:
+            selectionText = self.view.substr(region)
+            self.view.replace(edit, region, self.convert(selectionText))
+
+    def convert(self, text):
+        text = text.replace("'", '')
+        text = text.replace('"', '')
+        return re.sub(r'[\W_]+', '_', text).lower()
+
+class TransCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        selection = self.view.sel()
+        for region in selection:
+            selectionText = self.view.substr(region)
+            self.view.replace(edit, region, self.convert(selectionText))
+
+    def convert(self, text):
+        text = text.replace("'", '')
+        text = text.replace('"', '')
+        return "{{ __('translations." + re.sub(r'[\W_]+', '_', text).lower() + "') }}"
+
+
+class AssetingSourceCommand(sublime_plugin.TextCommand):
+    def run(self,edit):
+        def on_done(input_string):
+            self.view.run_command( 'asseting_source_helper', { 'location' : input_string } )
+
+        def on_change(input_string):
+            pass
+
+        def on_cancel():
+            print("User cancelled the input")
+                    
+        window = self.view.window()
+        window.show_input_panel("Text to Insert:", "", on_done, on_change, on_cancel)       
+    
+
+class AssetingSourceHelperCommand(sublime_plugin.TextCommand):
+    def run(self, edit, location = ''):
+        reg = sublime.Region(0, self.view.size())
+        content = self.view.substr(reg)
+        
+        links = re.findall(r'<link\s+(?:[^>]*?\s+)?href=(["\'])(.*?)\1', content)
+        for link in links:
+          content = content.replace(link[1], "{{ asset('" + location + "/" + link[1] +"') }}")
+
+        links = re.findall(r'<script\s+(?:[^>]*?\s+)?src=(["\'])(.*?)\1', content)
+        for link in links:
+          content = content.replace(link[1], "{{ asset('" + location + "/" + link[1] +"') }}")
+
+        links = re.findall(r"\<img.+src\=(?:\"|\')(.+?)(?:\"|\')(?:.+?)\>", content)
+        for link in links:
+          content = content.replace(link, "{{ asset('" + location + "/" + link +"') }}")
+
+        content = content.replace("{{ asset('"+location+"/{{", "{{")
+        content = content.replace("}}') }}", "}}")
+        content = content.replace("{{ asset('"+location+"/{{ asset('"+location+"/{{", "{{")
+        content = content.replace("}}') }}') }}", "}}")              
+        
+        self.view.erase(edit, reg)
+        self.view.insert(edit, 0, content)
